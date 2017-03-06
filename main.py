89f68803861c7730bcf65c946edc79b8c2af964f
guffy020 <guffy020@gmail.com>
2017-03-06 17:48:13 -0600

    [finished.. please grade]

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-print("welcome to the dungon. You cannot go back through the same way you came because the door is bulted shut from the outside. Your welcome for that by the way. You must find a way to escape. The only tool you have is your axe, if you ever want to use your axe type -axe-")
-print("You start walking and find your self in a big room. The room has three doors. What door do you want to go through?")
+print("Hey you, wake up...good.. I have kidnapped you from your home and brought you to my warehouse. Dont worry, im not going to kill you. You will do that to yourself.  You cannot go back through the same way you came because the door is bulted shut from the outside. Your welcome for that by the way. You must find a way to escape... if you do your free to go. Heres an axe you might need it, if you ever want to use your axe type -axe- GOOD LUCK")
+print("You start walking and find yourself in a big room. The room has three doors. What door do you want to go through?")
 door =raw_input("-first door- second door- or -third door-?")
 while(door != "third door"):
-    print("incorrect choose again")
+    print("Door is locked choose again")
     door =raw_input("-first door- second door- or -third door-?")
-print(" Correct... You have picked the right door.. You start walking and find yourself in a dark hallway.")
+print(" Correct... You have picked the right door.. You start walking and find yourself in a dark hallway. Do you hear that. It sounds like snoring.")
 
 secondchoice =raw_input("Do You -keep walking- or try to -find a light source-? Be carful you dont want to wake something up!")
 if(secondchoice == "find a light source"):
@@ -16,15 +16,15 @@ elif(secondchoice == "keep walking"):
         print("Didnt work. Try again") 
         halldoor =raw_input("HINT; What chops wood?")
     if(halldoor == "axe"):
-        print("Correct: you knocked down the door. YOu find yourself walking in a huge empty room. you keep walking and fin yourself in the middle of the room.WAIT whats that. My guard has found you.")
+        print("Correct: you knocked down the door. You find yourself walking in a huge empty room. You keep walking and find yourself in the middle of the room. WAIT whats that behind you. My guard has found you. As he approches you, you notice a key and a map on his belt.")
         thirdchoice =raw_input("Do you fight or run?")
         if(thirdchoice == "run"):
             print("you have escaped the guard. but when he was chacing you he fell in a hole and died. He was the only one with the keys to escape.CONGRATS GAME OVER")
         elif(thirdchoice == "fight"):
             print("Ok. you want to fight. All you have is your axe. He has full armor on but not a helmet. You can try to hit him with the axe or throw the axe")
-            fight =raw_input("throw or hit him with the axe")
+            fight =raw_input("throw or hit--")
             if(fight == "hit"):
-                print("You hit him. He has armor and hits you back you died")
+                print("You walked up to him to try and hit him. when you got close to him he hit you and you died")
             elif(fight == "throw"):
                 print("where do you want to throw?")
                 throw =raw_input("Head- chest-?")
